4600201b0a4b67399bc8b68567ad113cb5c177ce
Randy Reyes <rreyes@netvip.net>
2017-07-12 10:58:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# innatemotionfitness.com
+
+My client is interested in fusing his background in sports science and physical therapy with his love for dance to develop injury prevention and training strategies for break-dancers.
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang='en'>
+	<head>
+		<meta charset='utf-8'>
+		<meta name='robots' content='noindex, nofollow'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<title>Randy Q Reyes | </title>
+		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'>
+		<link rel='stylesheet' href='styles/style.css'>
+	</head>
+	<body>
+
+		<script src='https://code.jquery.com/jquery-3.1.0.min.js' integrity='sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=' crossorigin='anonymous'></script>
+		<script src='scripts/script.js'></script>
+	</body>
+</html>
